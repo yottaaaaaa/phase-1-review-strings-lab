b7a2e42fd8fcd4de20791352b706e224b8f51910
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-04-05 12:28:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'string';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
